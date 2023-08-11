c2828096384e0a938aba6fd8e6a6a9da9a1c6de5
valeriaaa12 <valeriaromero1275@gmail.com>
2023-08-11 13:38:23 -0600

    prueba 2

--- src/lab4p2_equipo10/Lab4P2_Equipo10.java
+++ src/lab4p2_equipo10/Lab4P2_Equipo10.java
@@ -14,7 +14,7 @@ public class Lab4P2_Equipo10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String pikachu;
     }
     
 }
