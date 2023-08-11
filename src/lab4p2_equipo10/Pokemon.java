8ecf48b2f9ebfde543fd3962e20d597ace05bcb2
valeriaaa12 <valeriaromero1275@gmail.com>
2023-08-11 13:47:38 -0600

    ahora si empece pokemon

--- src/lab4p2_equipo10/Pokemon.java
+++ src/lab4p2_equipo10/Pokemon.java
@@ -9,5 +9,16 @@ package lab4p2_equipo10;
  * @author rodge
  */
 public class Pokemon {
+    protected String especie;
+    protected int nivel;
+    protected int experiencia;
+    protected int subir_experiencia;
+    protected Movimiento[]  movimientos = new Movimiento[4];
+    protected int puntosVida;
+    protected int puntosAtaque;
+    protected int puntosDefensa;
+    protected int puntosEspecial;
+    protected int puntosVelocidad;
+    protected String estado;
     
 }
