147979dc5ae12accb22daac808bdd429ff4d2239
Diego <diegomartinvasquezfunez008@gmail.com>
2023-08-11 14:54:26 -0600

    Avance en el Main

--- src/lab4p2_equipo10/Lab4P2_Equipo10.java
+++ src/lab4p2_equipo10/Lab4P2_Equipo10.java
@@ -15,9 +15,10 @@ public class Lab4P2_Equipo10 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         Scanner leer = new Scanner(System.in);
+        ArrayList<Entrenador> entrenadores = new ArrayList();
         ArrayList<Movimiento> movimientos = new ArrayList();
         boolean seguir = true;
         while(seguir){
@@ -31,7 +32,15 @@ public class Lab4P2_Equipo10 {
             int opcion = leer.nextInt();
             switch(opcion){
                 case 1:{
-                    
+                    System.out.println("Ingrese el nombre del Entrenador:");
+                    String nombre = sc.nextLine();
+                    System.out.println("Ingrese la edad: ");
+                    int edad = leer.nextInt();
+                    System.out.println("Ingrese cuantos PokeDolares tiene: ");
+                    int PD = leer.nextInt();
+                    Entrenador pd = new Entrenador(nombre, edad, edad);
+                    entrenadores.add(pd);
+                    System.out.println("¡Entrenador Agregado Exitosamente!");
                 }//fin case
                 break;
                 case 2:{
@@ -48,7 +57,8 @@ public class Lab4P2_Equipo10 {
                         int opc = leer.nextInt();
                         switch(opc){
                             case 1:{
-                                
+                                //capturar pokemon
+                                System.out.println("");
                             }//fin case
                             break;
                             case 2:{
@@ -68,7 +78,82 @@ public class Lab4P2_Equipo10 {
                 }//fin case
                 break;
                 case 4:{
-                    
+                    //agregar movimiento
+                    boolean seg = true;
+                    while(seg){
+                        System.out.println("""
+                                           =AGREGAR MOVIMIENTO=
+                                           Ingrese que Tipo de Movimiento
+                                           1.Estado
+                                           2.Fisico
+                                           3.Especial
+                                           4.Salir de Movimientos""");
+                        int opc = leer.nextInt();
+                        switch(opc){
+                            case 1:{
+                                System.out.println("Ingrese el nombre del movimiento: ");
+                                String nombre = sc.nextLine();
+                                System.out.println("Ingrese la descripcion del movimiento: ");
+                                sc = new Scanner(System.in);
+                                String descripcion = sc.nextLine();
+                                String stat = "";
+                                boolean statusE = true;
+                                while(statusE){
+                                    System.out.println("""
+                                                       Ingrese que problema de estado afectara el movimiento:
+                                                       1.dormido
+                                                       2.envenenado
+                                                       3.paralizado
+                                                       4.quemado
+                                                       5.neutral""");
+                                    int status = leer.nextInt();
+                                    switch(status){
+                                        case 1:{
+                                            stat = "dormido";
+                                            statusE = false;
+                                        }//fin case
+                                        break;
+                                        case 2:{
+                                            stat = "envenenado";
+                                            statusE = false;
+                                        }//fin case
+                                        break;
+                                        case 3:{
+                                            stat = "paralizado";
+                                            statusE = false;
+                                            
+                                        }//fin case
+                                        break;
+                                        case 4:{
+
+                                        }//fin case
+                                        break;
+                                        case 5:{
+
+                                        }//fin case
+                                        break;
+                                        default:{
+                                            System.out.println("Opcion invalida, intentelo de nuevo:");
+                                        }//fin default
+                                        break;
+                                    }//fin switch
+                                }//fin while
+                            }//fin case
+                            break;
+                            case 2:{
+                                
+                            }//fin case
+                            break;
+                            case 3:{
+                                
+                            }//fin case
+                            break;
+                            case 4:{
+                                seg = false;
+                            }//fin case
+                            break;
+                        }//fin switch
+                    }//fin while
                 }//fin case
                 break;
                 case 5:{
